5ab1857a7d7f8c4875f2877ca26df111779fae47
RichardBTon <richard.btondel@gmail.com>
2020-10-23 15:42:56 +0200

    ProjectDisplay init

--- client/svelte/src/App.svelte
+++ client/svelte/src/App.svelte
@@ -1,17 +1,43 @@
 
 <script>
-	import Navbar from "./Navbar.svelte"
+	import Navbar from "./Navbar.svelte";
+	import ProjectDisplay from "./ProjectDisplay.svelte";
 
-	export let name = "på deg!";
+	let projectDisplays = [
+		{}
+	]
 </script>
+
 <div class="navbar">
 	<Navbar backgroundColor="#7B91F0"/>
 </div>
+<div class="main-wrapper">
+	<div class="display">
+		<ProjectDisplay newProject={true}/>
+
+		{#each displays as display}
+			<ProjectDisplay/>
+		{/each}
+	</div>
+</div>
+
 
 
 <style>
 	.navbar {
 		position: fixed;
 		width: 100%;
+		z-index: 10;
+	}
+
+	.main-wrapper {
+		padding-top: 150px;
+		width: 80%;
+		margin: 0 auto;
+	}
+	.display {
+		display: grid;
+		grid-gap: 40px;
+		grid-template-columns: 1fr 1fr 1fr;
 	}
 </style>
--- client/svelte/src/ProjectDisplay.svelte
+++ client/svelte/src/ProjectDisplay.svelte
@@ -1,7 +1,51 @@
 <script>
-
+  export let title = "Prosjekt uten navn"
+  export let newProject = false;
 </script>
 
-<style>
+{#if !newProject}
+<main>
+  <h2>{title}</h2>
+</main>
 
+
+{:else}
+<main>
+  <div class="plus-sign">
+    <svg xmlns="http://www.w3.org/2000/svg" class="iconic-plus-thin injected-svg iconic iconic-lg iconic-main" width="128" height="128" viewBox="0 0 128 128" data-src="/iconic/svg/plus-thin.svg">
+      <g class="iconic-plus-thin-lg iconic-container iconic-lg" data-width="128" data-height="128" display="inline">
+        <path stroke="#000" stroke-width="2" stroke-linecap="square" class="iconic-property-stroke" d="M64 4v120m60-60h-120" fill="none"></path>
+      </g>
+    </svg>
+  </div>
+
+</main>
+{/if}
+
+<style>
+  main {
+    position: relative;
+    border: solid 1px #333;
+    width: 300px;
+    height: 300px;
+    box-shadow: 0px 0px 7px 0px rgba(0,0,0,0.75);
+    transition: 0.3s;
+  }
+  h2 {
+    text-transform: uppercase;
+    font-weight: lighter;
+    font-size: 2em;
+    text-align: center;
+  }
+  .plus-sign {
+    position: absolute;
+    width: 128px;
+    height: 128px;
+    top: calc(50% - 64px);
+    left: calc(50% - 64px);
+  }
+  main:hover {
+    box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.75);
+    cursor: pointer;
+  }
 </style>
