b3a2596157d44b06d331e56e804ae38012e0582b
RichardBTon <richard.btondel@gmail.com>
2020-11-01 14:53:38 +0100

    Update DeltakerDisplay.svelte

--- client/svelte/src/DeltakerDisplay.svelte
+++ client/svelte/src/DeltakerDisplay.svelte
@@ -2,20 +2,35 @@
     export let deltaker = {
         navn: "Navnløs",
     };
+
+    let muted = false;
+    function handleMute(e) {
+        if (!muted) {
+            e.target.innerText = "Muted";
+        }
+        else {
+            e.target.innerText = "Mute";
+        };
+        muted = !muted;
+    }
+    // todo:
+    // Alt det naturlige
+    // Bytte ut tekst med et ikon med mute og muted
 </script>
 <main>
     <h3>{deltaker.navn}</h3>
-    <button class="mute-btn">Mute</button>
+    <button class="mute-btn" on:click={handleMute}>Mute</button>
 </main>
 <style>
     main {
         margin: 10px;
-        padding: 5px;
+        padding: 5px 30px;
         /* text-align: center; */
-        border: 1px solid black;
+        border: 1px solid var(--dark);
     }
     .mute-btn {
-        border: 1px solid black;
+        /* margin-left: ; */
+        border: 1px solid var(--dark);
         background: var(--dark);
         color: var(--light);
         padding: 5px 10px;
