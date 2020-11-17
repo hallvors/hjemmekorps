454620dc507b533ba59d620233a47ae62ad8e9ef
RichardBTon <richard.btondel@gmail.com>
2020-11-17 15:27:14 +0100

    cleaned up

--- client/svelte/ProjectHome/ProjectHome.svelte
+++ client/svelte/ProjectHome/ProjectHome.svelte
@@ -52,12 +52,16 @@
             <div class="merged-audio">
                 <h1 style="text-align: center;">Samlet lydfil</h1>
                 <span class="audio">
-                    <audio controls src="../sample.mp3">
+                    <audio controls src="../samples/sample.mp3">
                         <track kind="captions"/>
                         Audio does not work
                     </audio>
                 </span>
 
+            </div>
+
+            <div class="standard-box note-box">
+                <h1>Her skal det bli noter</h1>
             </div>
         </div>
         
@@ -124,6 +128,17 @@
         padding-bottom: 30px;
         border: var(--border);
         box-shadow: var(--shadow);
+    }
+
+    .note-box {
+        margin-top: 30px;
+        padding: 5%;
+        height: 20vw;
+    }
+
+    .note-box h1 {
+        padding: 0;
+        margin: 0;
     }
 
 
