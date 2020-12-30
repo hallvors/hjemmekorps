69c6b1aea125c15dddd3961e1ee73a753983ab89
Hallvord R. M. Steen <hallvord@minus.no>
2020-12-30 01:43:56 +0100

    Remove debug code

--- src/routes/api/mxml/[id].js
+++ src/routes/api/mxml/[id].js
@@ -4,11 +4,8 @@ import sClient from "../../../lib/sanity_client";
 
 export async function get(req, res, next) {
 	return sClient.getProject(req.user._id, req.params.id).then((project) => {
-		console.log('getting sheet music', project)
 		return got(project.sheetmusicFile)
 		.then(response => {
-			console.log('got response')
-			console.log(response.body)
 			res.setHeader('Content-type', 'application/vnd.recordare.musicxml+xml; charset=utf-8');
 			res.end(response.body);
 		});
