721a8e9e5cdec6d60ba8e49a776a1999be4baa4a
Hallvord R. M. Steen <hallvord@minus.no>
2020-12-30 01:32:43 +0100

    Adding info and privacy pages, need polish

---
+++ src/routes/om/hjemmekorps.svelte
@@ -0,0 +1,6 @@
+<p>
+Hjemmekorps skal inspirere til hjemmeøving ved å gjøre leksene til en del av felles prosjekter. Øve-opptak hjemmefra settes automatisk sammen til små låter som kan spilles av på neste samspill-øvelse. Det eneste dirigenten trenger å gjøre, er å laste opp noter i MusicXML-format.
+</p>
+<p>
+For å få bruke Hjemmekorps til ditt korps eller orkester, ta kontakt.
+</p>
---
+++ src/routes/om/personvern.svelte
@@ -0,0 +1,14 @@
+<h1>Bruk av personlige opplysninger</h1>
+<p>
+Hjemmekorps samler bare inn et minmum av personopplysninger, som er nødvendige for å drive tjenesten. Vi lagrer bare data i din nettleser for å huske at du er logget inn. Vi bruker ingen tredjeparts-tjenester for analyse eller sporing.
+</p>
+<p>
+Data om prosjekter og brukere lagres i Sanity.io, som er vår eneste leverandør.
+</p>
+<p>
+Etter personopplysningslovens § 18, 1. ledd, har du på forespørsel rett til innsyn i egne opplysninger. Du har også rett til å be om at informasjon slettes eller rettes.
+</p>
+
+<p>
+Dersom du har ytterligere spørsmål om Hjemmekorps og personvern, eller ønsker å bruke systemet for ditt korps eller orkester, ta kontakt.
+</p>
