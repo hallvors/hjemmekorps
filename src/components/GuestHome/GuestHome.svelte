3669517d315d6a4958880646a8f8ea46d3bd4fd8
Hallvord R. M. Steen <hallvord@hallvord.com>
2021-01-12 00:13:50 +0100

    ws

--- src/components/GuestHome/GuestHome.svelte
+++ src/components/GuestHome/GuestHome.svelte
@@ -1,1 +1,4 @@
-<p>Her kommer det til å stå litt mere om hva som er bra med prosjektet, til glede for tilfeldige besøkende</p>
+<p>
+    Her kommer det til å stå litt mere om hva som er bra med prosjektet, til
+    glede for tilfeldige besøkende.
+</p>
