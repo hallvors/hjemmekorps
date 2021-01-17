883cc3b2245c24b4789397dab22e5e6f4cc76975
Hallvord R. M. Steen <hallvord@hallvord.com>
2021-01-17 02:11:23 +0100

    Better formatted deltager display, hide mute button if no recording

--- src/components/ProjectMain/DeltakerDisplay.svelte
+++ src/components/ProjectMain/DeltakerDisplay.svelte
@@ -1,100 +1,111 @@
 <script>
+  let muted = false;
 
-    let muted = false;
+  function toggleMute(e) {
+    const button = e.currentTarget;
 
-    function toggleMute(e) {
-        const button = e.currentTarget;
-
-        if (!muted) {
-            button.firstChild.classList.toggle("fa-volume-mute");
-            button.firstChild.classList.toggle("fa-volume-up");
-
-        }
-        else {
-            button.firstChild.classList.toggle("fa-volume-mute");
-            button.firstChild.classList.toggle("fa-volume-up");
-        };
-        muted = !muted;
-
-
+    if (!muted) {
+      button.firstChild.classList.toggle("fa-volume-mute");
+      button.firstChild.classList.toggle("fa-volume-up");
+    } else {
+      button.firstChild.classList.toggle("fa-volume-mute");
+      button.firstChild.classList.toggle("fa-volume-up");
     }
+    muted = !muted;
+  }
 
-    export let newMember = false;
+  export let newMember = false;
 
-    export let member = {
-        navn: "Navnløs",
-    };
-
+  export let member = {
+    navn: "Navnløs",
+  };
 </script>
 
 {#if !newMember}
-<main>
+  <main>
     <h3>{member.name}</h3>
-    <button class="mute-btn" on:click|preventDefault={toggleMute}><i class="fas fa-volume-up"></i></button>
-    {member.name}
-</main>
-
+    {#if member.recording}
+      <button class="mute-btn" on:click|preventDefault={toggleMute}
+        ><i class="fas fa-volume-up" /></button
+      >
+    {/if}
+  </main>
 {:else}
-<main class="new-member">
+  <main class="new-member">
     <div class="plus-sign">
-        <svg xmlns="http://www.w3.org/2000/svg" class="iconic-plus-thin injected-svg iconic iconic-lg iconic-main" width="80" height="80" viewBox="0 0 128 128" data-src="/iconic/svg/plus-thin.svg">
-        <g class="iconic-plus-thin-lg iconic-container iconic-lg" data-width="128" data-height="128" display="inline">
-          <path stroke="#000" stroke-width="2" stroke-linecap="square" class="iconic-property-stroke" d="M64 4v120m60-60h-120" fill="none"></path>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="iconic-plus-thin injected-svg iconic iconic-lg iconic-main"
+        width="80"
+        height="80"
+        viewBox="0 0 128 128"
+        data-src="/iconic/svg/plus-thin.svg">
+        <g
+          class="iconic-plus-thin-lg iconic-container iconic-lg"
+          data-width="128"
+          data-height="128"
+          display="inline">
+          <path
+            stroke="#000"
+            stroke-width="2"
+            stroke-linecap="square"
+            class="iconic-property-stroke"
+            d="M64 4v120m60-60h-120"
+            fill="none"
+          />
         </g>
       </svg>
     </div>
-
-</main>
+  </main>
 {/if}
 
 <style>
-    main {
-        min-height: 118px;
-        margin: 20px;
-        padding: 5px 20px;
-        /* text-align: center; */
-        border: var(--border);
-        box-shadow: var(--shadow);
+  main {
+    min-height: 118px;
+    margin: 20px;
+    padding: 5px 20px;
+    /* text-align: center; */
+    border: var(--border);
+    box-shadow: var(--shadow);
 
-        transition: 0.3s;
-    }
+    transition: 0.3s;
+  }
 
-    h3 {
-        text-transform: none;
-    }
+  h3 {
+    text-transform: none;
+  }
 
-    main:hover {
-        cursor: pointer;
-        box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.75);
-    }
+  main:hover {
+    cursor: pointer;
+    box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.75);
+  }
 
-    .new-member {
-        position: relative;
-    }
+  .new-member {
+    position: relative;
+  }
 
-    /* .new-member:hover {
+  /* .new-member:hover {
         cursor: pointer;
         box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.75);
     } */
 
-    .plus-sign {
-        position: absolute;
-        /* 40px is half of plus sign-width and height */
-        top: calc(50% - 40px);
-        left: calc(50% - 40px);
-    }
-    .mute-btn {
-        border: 1px solid var(--dark);
-        background: var(--dark);
-        color: var(--textColorLight);
-        padding: 5px 10px;
-        border-radius: 100px;
-    }
+  .plus-sign {
+    position: absolute;
+    /* 40px is half of plus sign-width and height */
+    top: calc(50% - 40px);
+    left: calc(50% - 40px);
+  }
+  .mute-btn {
+    border: 1px solid var(--dark);
+    background: var(--dark);
+    color: var(--textColorLight);
+    padding: 5px 10px;
+    border-radius: 100px;
+  }
 
-    .mute-btn:hover {
-        cursor: pointer;
-        background: none;
-        color: var(--blue);
-    }
-
+  .mute-btn:hover {
+    cursor: pointer;
+    background: none;
+    color: var(--blue);
+  }
 </style>
