bc66048cf4120b013caf179541d328edfc654637
Hallvord R. M. Steen <hallvord@hallvord.com>
2021-01-26 12:23:45 +0100

    OSMD experiments

--- src/routes/osmd-test.svelte
+++ src/routes/osmd-test.svelte
@@ -1,88 +1,108 @@
-
-<button on:click="{tryPlay}">spill</button>
-
-<div id="sheetmusic-test"></div>
 <script>
-import { onMount } from "svelte";
+  import { onMount } from 'svelte';
+  import { createEventDispatcher } from "svelte";
 
-let project = {
-    _id: '80d2f2a9-69a3-4623-8bbe-e5c6b0842d2e'
-}
-let bpm = 100;
-let memberName = 'Peter';
+  const dispatch = createEventDispatcher();
 
-let sheetMusicRenderer;
-let playing = false;
+  let project = {
+    _id: 'w3J2fgMuhjI3JWEP60jjkT',
+  };
+  let bpm = 100;
+  let trackName = 'Flute';
 
-function tryPlay() {
+  let sheetMusicRenderer;
+  let playing = false;
+
+  function tryPlay() {
     if (!playing) {
-        sheetMusicRenderer.cursor.show();
-        if (sheetMusicRenderer.sheet.HasBPMInfo) {
-            bmp = sheetMusicRenderer.sheet.DefaultStartTempoInBpm;
-        }
-        playing = true;
-        scheduleNext();
+      sheetMusicRenderer.cursor.show();
+      if (sheetMusicRenderer.sheet.HasBPMInfo) {
+        bpm = sheetMusicRenderer.sheet.DefaultStartTempoInBpm;
+      }
+      playing = true;
+      scheduleNext();
     } else {
-        playing = false;
+      playing = false;
     }
-}
+  }
 
-function scheduleNext() {
+  let repetitions = [];
+
+  function scheduleNext() {
     let notes = sheetMusicRenderer.cursor.NotesUnderCursor();
-    console.log({notes: notes.length})
+    console.log({ notes: notes.length });
     let minValue = Number.POSITIVE_INFINITY;
-    let beatNote = 1 / sheetMusicRenderer.sheet.SheetPlaybackSetting.rhythm.denominator;
+    let beatNote =
+      1 / sheetMusicRenderer.sheet.SheetPlaybackSetting.rhythm.denominator;
     notes.forEach(note => {
-        let thisValue = note.length.numerator / note.length.denominator;
-        if (thisValue < minValue) {
-            minValue = thisValue;
-        }
+      let thisValue = note.length.numerator / note.length.denominator;
+      if (thisValue < minValue) {
+        minValue = thisValue;
+      }
     });
-    console.log(minValue)
+    //console.log(minValue);
     let delayMS = (minValue / beatNote) * (60000 /* ms in minute */ / bpm);
-    console.log({delayMS})
-    if ( /*sheetMusicRenderer.cursor.iterator.EndReached ||*/ delayMS === Number.POSITIVE_INFINITY ||  !playing) {
-        console.log('THE END');
-        return;
+    //console.log({ delayMS });
+    if (
+      /*sheetMusicRenderer.cursor.iterator.EndReached ||*/ delayMS ===
+        Number.POSITIVE_INFINITY ||
+      !playing
+    ) {
+      console.log('THE END');
+      dispatch("finished", {});
+      return;
     }
-    setTimeout(nextNote, delayMS);
-}
 
-function nextNote() {
+    // Repetition?
+    if (
+      sheetMusicRenderer.cursor.Iterator.currentMeasure
+        .firstRepetitionInstructions
+    ) {
+      console.log(
+        sheetMusicRenderer.cursor.Iterator.currentMeasure
+          .firstRepetitionInstructions
+      );
+      repetitions.push({
+        start:
+          sheetMusicRenderer.cursor.Iterator.currentMeasure
+            .firstRepetitionInstructions,
+        end: null,
+        count: 0,
+      });
+    }
+
+    setTimeout(nextNote, delayMS);
+  }
+
+  function nextNote() {
     sheetMusicRenderer.cursor.next();
     scheduleNext();
-}
+  }
 
-onMount(async() => {
-        const module = await import('opensheetmusicdisplay');
+  onMount(async () => {
+    const module = await import('opensheetmusicdisplay');
 
-        sheetMusicRenderer = new module.default.OpenSheetMusicDisplay('sheetmusic-test', {
-            autoResize: true,
-            drawTitle: true,
-            drawingParameters: 'compact',
-            drawPartNames: true,
-            disableCursor: false,
-
+    sheetMusicRenderer = new module.default.OpenSheetMusicDisplay(
+      'sheetmusic-test',
+      {
+        autoResize: true,
+        drawTitle: true,
+        drawingParameters: 'compact',
+        drawtrackNames: true,
+        disableCursor: false,
+        followCursor: true,
+      }
+    );
+    fetch(`/api/project/${project._id}/score/${encodeURIComponent(trackName)}`)
+      .then(data => data.text())
+      .then(xmlSource => {
+        sheetMusicRenderer.load(xmlSource).then(() => {
+          sheetMusicRenderer.render();
         });
-        fetch('/api/mxml/' + project._id)
-        .then(data => data.text())
-        .then(xmlSource => {
-            let parser = new DOMParser();
-            let xmlDoc = parser.parseFromString(xmlSource,"text/xml");
-            window.doc = (xmlDoc)
-
-            sheetMusicRenderer.load(xmlDoc)
-            .then(() => {
-                sheetMusicRenderer.sheet.instruments.forEach(instrument => {
-                    console.log(instrument.nameLabel.text)
-                    instrument.Visible = instrument.nameLabel.text === memberName ||
-                        instrument.nameLabel.text.indexOf(memberName) > -1
-                });
-                sheetMusicRenderer.render();
-            });
-        });
-        //sheetMusicRenderer.load('/api/mxml/' + project._id)
-        //.then(() => sheetMusicRenderer.render());    
-})
-
+      });
+  });
 </script>
+
+<button on:click={tryPlay}>spill</button>
+
+<div id="sheetmusic-test" />
