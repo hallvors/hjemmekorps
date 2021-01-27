deb16daf1f0a5325c33e2f1d5254be4981c460b4
Hallvord R. M. Steen <hallvord@hallvord.com>
2021-01-27 02:32:36 +0100

    Fix merge conflict in _layout, set max-width in vw

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,35 +1,34 @@
 <script>
-	// This organises the basic layout for all pages:
-	// - navbar w logo
-	// - slot for main content
-	// - footer
-	// _layout can _not_ assume that a user session exists.
-	// Some pages (/feil/.., /om/..) are available without one
+  // This organises the basic layout for all pages:
+  // - navbar w logo
+  // - slot for main content
+  // - footer
+  // _layout can _not_ assume that a user session exists.
+  // Some pages (/feil/.., /om/..) are available without one
 
-	import Navbar from '../components/Navbar/Navbar.svelte';
-	import Footer from '../components/Footer/Footer.svelte';
+  import Navbar from '../components/Navbar/Navbar.svelte';
+  import Footer from '../components/Footer/Footer.svelte';
 
-	// segment will be automatically set to last part of the URL in case
-	// of nested routes - https://sapper.svelte.dev/docs#Nested_routes
-	//export let segment;
-
+  // segment will be automatically set to last part of the URL in case
+  // of nested routes - https://sapper.svelte.dev/docs#Nested_routes
+  //export let segment;
 </script>
-
-<style>
-	main {
-		position: relative;
-		max-width: 56em;
-		background-color: var(--light);
-		padding: 2em;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-</style>
 
 <Navbar />
 
 <main>
-	<slot></slot>
+  <slot />
 </main>
 
 <Footer />
+
+<style>
+  main {
+    position: relative;
+    max-width: 70vw;
+    background-color: var(--light);
+    padding: 2em;
+    margin: 0 auto;
+    box-sizing: border-box;
+  }
+</style>
