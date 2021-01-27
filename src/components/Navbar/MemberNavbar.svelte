2e0a4ba945f2b434315601cf0140e982753114b1
RichardBTon <richard.btondel@gmail.com>
2021-01-27 11:01:12 +0100

    Navbar cleanup

---
+++ src/components/Navbar/MemberNavbar.svelte
@@ -0,0 +1,19 @@
+<script>
+
+	import { stores } from '@sapper/app';
+	const { session } = stores();
+
+	let user;
+	session.subscribe(data => {
+		user = data.user;
+	});
+</script>
+
+<main>
+    <h1>Hei {user.name}!</h1>
+</main>
+<style>
+    main {
+        text-align: center;
+    }
+</style>
