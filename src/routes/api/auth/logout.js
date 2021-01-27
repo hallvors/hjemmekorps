a881f317fdb918353b90fd54e6f516206b536c9f
Hallvord R. M. Steen <hallvord@hallvord.com>
2021-01-28 00:24:16 +0100

    attempted fix for logging out, still does not set cookies

--- src/routes/api/auth/logout.js
+++ src/routes/api/auth/logout.js
@@ -1,4 +1,6 @@
-export function post(req, res, next) {
+export function get(req, res, next) {
 	res.setCookie('token', '', {expires: new Date(0)});
-	res.json({status: 'logged out'});
+	res.statusCode = 302;
+	res.setHeader('Location', '/');
+	res.end();
 }
