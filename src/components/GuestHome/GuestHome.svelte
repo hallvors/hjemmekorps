9eb40f209475a8b1ed9a3e7c1e64f1b011077674
Hallvord R. M. Steen <hallvord@minus.no>
2021-01-29 00:09:21 +0100

    Slightly more presentational text

--- src/components/GuestHome/GuestHome.svelte
+++ src/components/GuestHome/GuestHome.svelte
@@ -1,4 +1,11 @@
+
 <p>
-    Her kommer det til å stå litt mere om hva som er bra med prosjektet, til
-    glede for tilfeldige besøkende.
+	Hjemmekorps skal inspirere korpsmusikanter til hjemmeøving ved å gjøre leksene til en del av felles prosjekter. Dirigenten legger inn noter, musikantene spiller inn stemmene hjemme og får høre det kombinerte resultatet - enten rett etter innspilling eller på neste øvelse.
+</p>
+
+<p>...det kan også brukes under pandemier til å gi en liten fornemmelse av samspill..</p>
+
+<p>
+    <em>Her kommer det til å stå enda mere om hva som er bra med prosjektet, til
+    glede for tilfeldige besøkende.</em>
 </p>
