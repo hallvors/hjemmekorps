27a231fc3468d051723cc3d29a5ae5adee1a9425
Hallvord R. M. Steen <hallvord@minus.no>
2021-01-29 00:17:19 +0100

    Some more Om blurb

--- src/routes/om/hjemmekorps.svelte
+++ src/routes/om/hjemmekorps.svelte
@@ -1,8 +1,22 @@
+<h1>Om "Hjemmekorps"</h1>
+
 <p>
-Hjemmekorps skal inspirere til hjemmeøving ved å gjøre leksene til en del av felles prosjekter. Øve-opptak hjemmefra settes automatisk sammen til små låter som kan spilles av på neste samspill-øvelse. Det eneste dirigenten trenger å gjøre, er å laste opp noter i MusicXML-format.
+Hjemmekorps skal inspirere til hjemmeøving ved å gjøre leksene til en del av felles prosjekter. Det er ment som et verktøy for dirigenter og instruktører. Målet er selvfølgelig større mestringsfølelse og mindre frafall blandt de yngste - men det kan også brukes mot pandemi og sosial isolasjon for både små og store.
+</p>
+<p>
+ Øve-opptak hjemmefra settes automatisk sammen til små låter som kan spilles av enten rett etter opptaket eller på neste samspill-øvelse. Det eneste dirigenten trenger å gjøre, er å laste opp noter i MusicXML-format og sende ut lenker til musikantenes opptaks-sider.
 </p>
 <p>
 For å få bruke Hjemmekorps til ditt korps eller orkester, ta kontakt.
 </p>
 
-<div>Instrument-ikoner er lagd av <a href="http://www.freepik.com" title="Freepik">Freepik</a> og hentet fra <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+<h2>Rulletekster..</h2>
+<p>
+<b>Koding:</b> Hallvord Reiar Michaelsen Steen og Richard Birkeland Tøndel.
+</p>
+
+<p>
+<b>Hosting</b>: <a href="https://heroku.com">heroku.com</a> og <a href="https://sanity.io">sanity.io</a>.
+</p>
+
+<p><b>Instrument-ikoner</b>: <a href="http://www.freepik.com" title="Freepik">Freepik</a>, hentet fra <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></p>
