00cf8065954dcfbc06f71fabb5d7f153e31000c2
Hallvord R. M. Steen <hallvord@hallvord.com>
2021-01-31 00:34:05 +0100

    Add a loading animation - yay

---
+++ src/components/Loading/Loading.svelte
@@ -0,0 +1,75 @@
+<script>
+  import { instruments } from '../../lib/datastore';
+  import { getRandomInt } from '../../lib/utils';
+  let rndInstr = [];
+  function pickInstruments() {
+    for (let i = 0; i < 3; i++) {
+      rndInstr[i] = $instruments[getRandomInt(0, $instruments.length)];
+    }
+    setTimeout(pickInstruments, 8000);
+  }
+  pickInstruments()
+</script>
+
+<div class="loading">
+  {#each rndInstr as instrument, idx}
+    <img
+      class="i{idx}"
+      src="/images/instruments/{instrument.value}.png"
+      alt=""
+    />
+  {/each}
+<p>Vent litt...</p>
+</div>
+
+<style>
+  .loading {
+    position: relative;
+    width: 100%;
+    height: 100%;
+  }
+  p {
+      color: #999;
+    letter-spacing: 8%;
+    font-size: 5vw;
+    text-align: center;    
+  }
+  img {
+    width: 10%;
+    position: absolute;
+  }
+
+  img.i0 {
+    top: 35%;
+    left: 15%;
+  }
+  img.i1 {
+    top: 35%;
+    left: 45%;
+  }
+  img.i2 {
+    top: 35%;
+    left: 75%;
+  }
+  img {
+    -webkit-animation: spin 8s linear infinite;
+    -moz-animation: spin 8s linear infinite;
+    animation: spin 8s linear infinite;
+  }
+  @-moz-keyframes spin {
+    100% {
+      -moz-transform: rotate(360deg);
+    }
+  }
+  @-webkit-keyframes spin {
+    100% {
+      -webkit-transform: rotate(360deg);
+    }
+  }
+  @keyframes spin {
+    100% {
+      -webkit-transform: rotate(360deg);
+      transform: rotate(360deg);
+    }
+  }
+</style>
