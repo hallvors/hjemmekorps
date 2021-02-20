436b238a621c5fe239e97613e23425344c38a7a5
Hallvord R. M. Steen <hallvord@hallvord.com>
2021-02-21 00:54:13 +0100

    Helper file for sending form data

---
+++ src/components/utils/forms.js
@@ -0,0 +1,24 @@
+
+export function createSubmitHandler(cb) {
+    return function (evt) {
+        if (evt.target.checkValidity()) {
+          let data = {};
+          for (let i = 0; i<evt.target.elements.length; i++) {
+              data[evt.target.elements[i].name] = evt.target.elements[i].value;
+          }
+          return fetch(evt.target.action, {
+            method: 'POST',
+            credentials: 'same-origin',
+            body: JSON.stringify(data),
+            headers: {
+              Accept: 'application/json',
+              'Content-Type': 'application/json',
+            },
+          }).then(response => {
+            console.log(response);
+            cb(evt);
+          });
+        }
+      }
+
+}
