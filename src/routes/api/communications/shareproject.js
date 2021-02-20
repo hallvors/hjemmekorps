79593795b58d7865512143947a10409109dbf564
Hallvord R. M. Steen <hallvord@hallvord.com>
2021-02-20 17:31:09 +0100

    better api route for share project email

--- src/routes/api/communications/shareproject.js
+++ src/routes/api/communications/shareproject.js
@@ -1,4 +1,6 @@
-import sClient from '../../lib/sanity_client';
+import Handlebars from "handlebars";
+import sClient from '../../../lib/sanity_client';
+import {send} from '../../../lib/mail';
 
 export async function post(req, res, next) {
 	if (req.user && req.user._type === 'adminUser') {
@@ -13,14 +15,17 @@ export async function post(req, res, next) {
 				let token = part.members[j].token;
 				let member = await sClient.getUserData(part.members[j]._ref);
 				recipients.push({
-					email: member.email,
+					to: member.email,
 					name: member.name,
 					token,
+					
 				});
 			}
 		}
 		// req.body.message + blurb personalized to every receipient here
-
+		await Promise.all(recipients.map(recipient => {
+			send(recipient.to, '')
+		}));
 	}
 	// should never get here..
 	res.statusCode = 401;
