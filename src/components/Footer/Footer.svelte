7f286ed379c2495e274f8b8fc9808f9bd5ed16c2
Hallvord R. M. Steen <hallvord@hallvord.com>
2021-02-22 09:21:21 +0100

    center text in footer

--- src/components/Footer/Footer.svelte
+++ src/components/Footer/Footer.svelte
@@ -1,7 +1,14 @@
-
-
 <footer>
-	<p>
-		<small>Hjemmekorps - <a href="/om/hjemmekorps">Om</a> | <a href="/om/personvern">Personvern</a> </small>
-	</p>
+  <p>
+    <small
+      >Hjemmekorps - <a href="/om/hjemmekorps">Om</a> |
+      <a href="/om/personvern">Personvern</a>
+    </small>
+  </p>
 </footer>
+
+<style>
+  footer {
+    text-align: center;
+  }
+</style>
