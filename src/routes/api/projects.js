11222674a342e02326f5e0c400f6cf401776fc3c
Hallvord R. M. Steen <hallvord@hallvord.com>
2021-03-14 22:25:45 +0100

    Disable generating SVG on server (for now)

--- src/routes/api/projects.js
+++ src/routes/api/projects.js
@@ -3,6 +3,8 @@ import multer from 'multer';
 const storage = multer.memoryStorage();
 const multerUpload = multer({ storage: storage });
 
+const OPT_MAKE_SVG = false;
+
 import sClient from '../../lib/sanity_client';
 import { generateSVGImage } from '../../lib/mxml_to_svg';
 import {
@@ -42,30 +44,34 @@ export async function post(req, res, next) {
         )
         .then(project => {
           res.json(project);
-          // TODO: how to make Polka / Node HTTP server flush data HERE?
-          // user should not wait for the slow parts below..
-          // process parts, convert to SVG
-          return Promise.all(
-            project.partslist.map(async partinfo => {
-              const svgMarkup = await generateSVGImage(
-                req.file.buffer.toString('utf-8'),
-                partinfo.part,
-                800,
-                0,
-                true
+          if (OPT_MAKE_SVG) {
+            // TODO: how to make Polka / Node HTTP server flush data HERE?
+            // user should not wait for the slow parts below..
+            // process parts, convert to SVG. Hmm.. timeout?
+            setTimeout(function () {
+              return Promise.all(
+                project.partslist.map(async partinfo => {
+                  const svgMarkup = await generateSVGImage(
+                    req.file.buffer.toString('utf-8'),
+                    partinfo.part,
+                    800,
+                    0,
+                    true
+                  );
+                  if (svgMarkup[0]) {
+                    return await sClient.addPartFile(
+                      project._id,
+                      partinfo.part,
+                      Buffer.from(svgMarkup[0], 'utf8')
+                    );
+                  } else {
+                    console.error('No SVG generated for ' + partinfo.part);
+                    console.error(svgMarkup);
+                  }
+                })
               );
-              if (svgMarkup[0]) {
-                return await sClient.addPartFile(
-                  project._id,
-                  partinfo.part,
-                  Buffer.from(svgMarkup[0], 'utf8')
-                );
-              } else {
-                console.error('No SVG generated for ' + partinfo.part);
-                console.error(svgMarkup)
-              }
-            })
-          );
+            }, 1);
+          }
         });
     });
   });
