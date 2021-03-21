b1d88fce688e0ae502f98b368b117fcc462cac44
Hallvord R. M. Steen <hallvord@hallvord.com>
2021-03-21 12:35:28 +0100

    Make sure audio_helpers returns promise

--- src/lib/audio_helpers.js
+++ src/lib/audio_helpers.js
@@ -18,50 +18,61 @@ async function mergeSoundfiles(projectId) {
   // get recording.url
   const files = await Promise.all(
     recordings.map(recording => {
+      console.log('will get sound file ' + recording.url);
       return got(recording.url, { responseType: 'buffer' }).then(result => {
         const file = path.join(tmpDir.name, recording._id) + '.wav';
         const fHandle = fs.openSync(file, 'w');
         fs.writeFileSync(fHandle, result.body);
         fs.closeSync(fHandle);
+        console.log('done writing ', recording.url, file);
         return file;
       });
     })
   );
+  console.log('sound files ' + files.length);
   if (files.length === 1) {
-    sClient.addCombinedRecording(projectId, files[0]);
-    return;
+    return sClient.addCombinedRecording(projectId, files[0]);
   }
 
-  const command = ffmpeg();
-  // debug info..
-  command.on('start', function (commandLine) {
-    console.log('Spawned Ffmpeg with command: ' + commandLine);
-  });
-  command.on('error', function (error) {
-    console.error('Ffmpeg dies with error: ' + error);
-  });
-  command.on('codecData', function (data) {
-    console.log(
-      'ffmpeg input is ' + data.audio + ' audio ' + 'with ' + data.video + ' video'
-    );
-  });
-  files.forEach(file => {
-    command.input(file); // TODO: add setStartTime() based on analysis of wav file
-  });
-  // -filter_complex amix=inputs=NUM:duration=longest
-  command.complexFilter({
-    filter: 'amix',
-    options: { inputs: files.length, duration: 'longest' },
-  });
-  let filepath = path.join(tmpDir.name, 'combined.wav');
-  command.on('end', function (stdout, stderr) {
-    console.log('ffmpeg succeeded !');
-    console.log(stdout, stderr);
-    sClient.addCombinedRecording(projectId, filepath).then(() => {
-      tmpDir.removeCallback();
+  return new Promise((resolve, reject) => {
+    const command = ffmpeg();
+    // debug info..
+    command.on('start', function (commandLine) {
+      console.log('Spawned Ffmpeg with command: ' + commandLine);
     });
+    command.on('error', function (error) {
+      console.error('Ffmpeg dies with error: ' + error);
+      reject(error);
+    });
+    command.on('codecData', function (data) {
+      console.log(
+        'ffmpeg input is ' +
+          data.audio +
+          ' audio ' +
+          'with ' +
+          data.video +
+          ' video'
+      );
+    });
+    files.forEach(file => {
+      command.input(file); // TODO: add setStartTime() based on analysis of wav file
+    });
+    // -filter_complex amix=inputs=NUM:duration=longest
+    command.complexFilter({
+      filter: 'amix',
+      options: { inputs: files.length, duration: 'longest' },
+    });
+    let filepath = path.join(tmpDir.name, 'combined.wav');
+    command.on('end', function (stdout, stderr) {
+      console.log('ffmpeg succeeded !');
+      console.log(stdout, stderr);
+      sClient.addCombinedRecording(projectId, filepath).then(res => {
+        tmpDir.removeCallback();
+        resolve(res);
+      });
+    });
+    command.save(filepath);
   });
-  command.save(filepath);
 }
 
 module.exports = {
