8305bc4f220d8b6a4b1f1ccecdf776c333b7b902
Hallvord R. M. Steen <hallvord@hallvord.com>
2021-03-21 15:36:11 +0100

    Fix missing require

--- src/routes/api/project/[id]/recombine-recording.js
+++ src/routes/api/project/[id]/recombine-recording.js
@@ -1,3 +1,4 @@
+const Queue = require('bull');
 const REDIS_URL = process.env.REDIS_URL || 'redis://127.0.0.1:6379';
 // Create / Connect to a named work queue
 const workQueue = new Queue('ffmpeg-work', REDIS_URL);
