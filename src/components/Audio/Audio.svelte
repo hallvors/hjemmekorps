b5b0a718ffaa3aaef785d5765c68e871d2904d49
Hallvord R. M. Steen <hallvord@hallvord.com>
2021-03-21 13:52:18 +0100

    Add download links for recordings

--- src/components/Audio/Audio.svelte
+++ src/components/Audio/Audio.svelte
@@ -8,8 +8,16 @@
 </script>
 
 <div>
-  {#if recording}Stemme <em>{part}</em>, musikant
-    <em>{member.name}</em><br />
+  {#if recording}
+  <div class="about">
+    <span>Stemme <em>{part}</em>, musikant
+      <em>{member.name}</em></span>
+      <span class="dl-link">
+        <a href={recording.url + '?dl'}
+          ><i class="fa fa-download" aria-hidden="true" title="Last ned"></i>
+        </a>
+      </span>
+  </div>
     <!--
     TODO: a way to detect and save volume. Maybe custom UI?
     <input type="range" name="volume" bind:value={volume} min="0" max="1" step="0.05" />
@@ -28,5 +36,12 @@
 <style>
   audio {
     width: 100%;
+  }
+  .about {
+    display: flex;
+    justify-content: space-between;
+  }
+  .dl-link {
+    text-align: right;
   }
 </style>
