af170bbee38fd6403c0efcc04a830744c3409723
Hallvord R. M. Steen <hallvord@hallvord.com>
2021-03-26 22:32:39 +0100

    Forgotten component files..

---
+++ src/components/Forward/Forward.svelte
@@ -0,0 +1,13 @@
+<script>
+  export let href = '';
+  export let id = '';
+  if (id && !href) {
+    href = `/prosjekt/${id}`;
+  }
+</script>
+
+<p>
+  <a {href} on:click={() => history.back()}
+    >Videre <i class="fa fa-arrow-right" aria-hidden="true" />
+  </a>
+</p>
