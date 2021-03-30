23693e61d7ad0cc8ca13a2b9248aecfe23439992
Hallvord R. M. Steen <hallvord@hallvord.com>
2021-03-30 20:09:56 +0200

    simplify when-to-beat countdown, fixes #105

--- src/components/Metronome/Metronome.svelte
+++ src/components/Metronome/Metronome.svelte
@@ -89,7 +89,7 @@
       recordBeat = countDownBeats.length > 4;
     }
 
-    if (beatNumber % nthBeatSounded === 0 || countDownBeats.length) {
+    if (beatNumber % nthBeatSounded === 0) {
       let source = audioContext.createBufferSource();
       if (beatNumber === 0) {
         // 0th beat in measure == high pitch
