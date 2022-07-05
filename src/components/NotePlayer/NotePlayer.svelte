e293cd612ad61bfda97a8c382f2d3c305cc8f5ac
Hallvord R. M. Steen <hallvord@hallvord.com>
2022-07-05 18:39:03 +0200

    OSMD experiments, not yet hooked up to UI

--- src/components/NotePlayer/NotePlayer.svelte
+++ src/components/NotePlayer/NotePlayer.svelte
@@ -18,6 +18,7 @@
   const timeStart = Date.now();
   let playbackManager;
   let timingSource;
+  let audioPlayer;
   onMount(async function () {
     loadingMessage = 'Henter notene...';
     let request;
@@ -59,7 +60,7 @@
           drawingParameters: 'compact',
           cursorsOptions: [
             {
-              type: 0,
+              type: opensheetmusicdisplay.CursorType.Standard,
               color: '#000099',
               alpha: 0.4,
               follow: true,
@@ -83,20 +84,87 @@
         project: project._id,
         ms: Date.now() - perfmeasure,
       });
+      audioPlayer = new opensheetmusicdisplay.BasicAudioPlayer();
       timingSource = new opensheetmusicdisplay.LinearTimingSource();
       playbackManager = new opensheetmusicdisplay.PlaybackManager(
         timingSource,
         undefined,
-        new opensheetmusicdisplay.BasicAudioPlayer(),
+        audioPlayer,
         undefined
       );
+      playbackManager.Metronome.Volume = 0.5;
       playbackManager.DoPlayback = true;
       playbackManager.DoPreCount = true;
       playbackManager.PreCountMeasures = 1; // note that DoPreCount has to be true for a precount to happen
       timingSource.Settings = sheetMusicRenderer.sheet.playbackSettings;
       playbackManager.initialize(sheetMusicRenderer.sheet.musicPartManager);
       playbackManager.addListener(sheetMusicRenderer.cursor);
+      playbackManager.addListener({
+        cursorPositionChanged: (timestamp, data) => {
+          console.log('cursorpositionchanged', { timestamp, data });
+          dispatch('measuretime', {
+            time: audioContext.currentTime - startTime,
+          });
+        },
+        pauseOccurred: obj => {
+          console.log('paused', obj);
+          dispatch('ended');
+        },
+        selectionEndReached: obj => {
+          console.log('ended', obj);
+          dispatch('ended');
+        },
+        resetOccurred: obj => {
+          console.log('reset');
+        },
+        notesPlaybackEventOccurred: obj => {
+          console.log('playback event', obj);
+          dispatch('countdownUiUpdate');
+        },
+      });
+      sheetMusicRenderer.renderingManager.addListener({
+        userDisplayInteraction: (
+          relativePosition,
+          positionInSheetUnits,
+          type
+        ) => {
+          console.log('user interaction', {
+            relativePosition,
+            positionInSheetUnits,
+            type,
+          });
+          switch (type) {
+            case 0:
+            case 1:
+            case 2: {
+              const clickVe =
+                sheetMusicRenderer.renderingManager.GraphicalMusicSheet.GetNearestVoiceEntry(
+                  positionInSheetUnits
+                );
 
+              // set cursor and/or start/end marker position
+              if (clickVe) {
+                if (
+                  clickVe.parentStaffEntry.parentVerticalContainer !== undefined
+                ) {
+                  const clickedTimeStamp =
+                    clickVe.parentStaffEntry.parentVerticalContainer
+                      .AbsoluteTimestamp;
+                  sheetMusicRenderer.renderingManager.setStartPosition(
+                    clickedTimeStamp
+                  );
+                  playbackManager.setPlaybackStart(clickedTimeStamp);
+                }
+              }
+              break;
+            }
+            default:
+              // Do nothing
+              break;
+          }
+        },
+      });
+      //debugger;
       top.osmd = sheetMusicRenderer; // Debug. TODO: remove
     }
     renderingMusic = false;
