37f11cce27ef5605ce0719aa38af679b5a7343eb
Hallvord R. M. Steen <hallvord@hallvord.com>
2023-10-03 23:04:47 +0200

    Split first name and surname in members schema

--- sanity/schemas/member.js
+++ sanity/schemas/member.js
@@ -1,60 +1,81 @@
 import settings from '../hjemmekorps-settings/instruments';
 
 export default {
-  title: "Musikant",
-  name: "member",
-  type: "document",
+  title: 'Musikant',
+  name: 'member',
+  type: 'document',
   fields: [
     {
-      title: "Navn",
-      name: "name",
-      type: "string",
+      title: 'Navn',
+      name: 'name',
+      type: 'string',
     },
     {
-      title: "Telefon",
-      description: "Norske mobilnumre støttes",
-      name: "phone",
-      type: "array",
-      of: [{type: "string"}]
+      title: 'Etternavn',
+      name: 'surname',
+      type: 'string',
     },
     {
-      title: "E-post",
-      name: "email",
-      type: "array",
-      of: [{type: "email"}]
+      title: 'Telefon',
+      description: 'Norske mobilnumre støttes',
+      name: 'phone',
+      type: 'array',
+      of: [{ type: 'string' }],
     },
     {
-      title: "Portrett",
-      name: "portrait",
-      type: "image",
+      title: 'E-post',
+      name: 'email',
+      type: 'array',
+      of: [{ type: 'email' }],
     },
     {
-      title: "Instrument",
-      name: "instrument",
-      type: "string",
+      title: 'Portrett',
+      name: 'portrait',
+      type: 'image',
+    },
+    {
+      title: 'Instrument',
+      name: 'instrument',
+      type: 'string',
       options: {
         list: settings.instruments,
       },
     },
     {
-      title: "Korps",
-      name: "band",
-      type: "reference",
-      to: [{ type: "band" }],
+      title: 'Korps',
+      name: 'band',
+      type: 'reference',
+      to: [{ type: 'band' }],
     },
     {
-      title: "Undergruppe",
-      name: "subgroup",
-      type: "string",
-    }, 
+      title: 'Undergruppe',
+      name: 'subgroup',
+      type: 'string',
+    },
     {
-      title: "Aktiv",
-      description: "Musikanter som permitteres eller slutter kan de-aktiveres her",
-      name: "visible",
-      type: "boolean"
-    }
+      title: 'Aktiv',
+      description:
+        'Musikanter som permitteres eller slutter kan de-aktiveres her',
+      name: 'visible',
+      type: 'boolean',
+    },
   ],
   initialValue: {
-    visible: true
-  }
+    visible: true,
+  },
+  preview: {
+    select: {
+      name: 'name',
+      surname: 'surname',
+      subtitle: 'band.name',
+      group: 'subgroup',
+      visible: 'visible',
+    },
+    prepare: values => ({
+      title:
+        [values.name, values.surname].filter(Boolean).join(' ') +
+        (values.visible ? '' : ' [inaktiv]'),
+      subtitle: [values.subtitle, values.group].filter(Boolean).join(', '),
+    }),
+  },
 };
