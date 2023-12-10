b85d9fd0a31443082bae611f08c36bd02afa3ca2
Hallvord R. M. Steen <hallvord@minus.no>
2023-12-10 19:40:23 +0100

    Upgrade to Sanity studio 3

---
+++ sanity/sanity.config.js
@@ -0,0 +1,16 @@
+// sanity.config.js
+import { defineConfig } from 'sanity';
+import { deskTool } from 'sanity/desk';
+import { visionTool } from '@sanity/vision';
+
+import schemas from './schemas/schema';
+
+export default defineConfig({
+  title: 'Hjemmekorps',
+  projectId: '5ggq2na3',
+  dataset: 'test',
+  plugins: [deskTool(), visionTool()],
+  schema: {
+    types: schemas,
+  },
+});
--- sanity/schemas/schema.js
+++ sanity/schemas/schema.js
@@ -1,9 +1,3 @@
-// First, we must import the schema creator
-import createSchema from 'part:@sanity/base/schema-creator';
-
-// Then import schema types from any plugins that might expose them
-import schemaTypes from 'all:part:@sanity/base/schema-type';
-
 // Custom schemas
 import admin from './admin.js';
 import project from './project.js';
@@ -13,19 +7,12 @@ import recording from './recording.js';
 import projectassignment from './projectassignment.js';
 import timingmeta from './object-timingmeta.js';
 
-// Then we give our schema to the builder and provide the result to Sanity
-export default createSchema({
-  // We name our schema
-  name: 'default',
-  // Then proceed to concatenate our document type
-  // to the ones provided by any plugins that are installed
-  types: schemaTypes.concat([
-    admin,
-    project,
-    band,
-    member,
-    recording,
-    projectassignment,
-    timingmeta,
-  ]),
-});
+export default [
+  admin,
+  project,
+  band,
+  member,
+  recording,
+  projectassignment,
+  timingmeta,
+];
